0c76bcce5b998815e9fadd3ff186eeeefd8bf5c8
mahadih-gg <mahadih.dev@gmail.com>
2025-04-12 00:00:28 +0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, corporis accusamus. Molestiae saepe perspiciatis itaque est, aspernatur vel optio sit accusantium eum quis maiores cupiditate! Voluptas at quia dolorem exercitationem.
+      yo yo
     </div>
   );
 }
