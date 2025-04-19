5fbca53d464681eab362e9104fae9890c8cbaa17
mahadih-gg <mahadih.dev@gmail.com>
2025-04-19 09:25:56 +0600

    Update HeroSlider component to extend the interval duration for automatic slide transitions from 3 seconds to 10 seconds; enhance visual effects by adjusting opacity transitions for active indicators and text.

--- src/components/global/HeroSlider.tsx
+++ src/components/global/HeroSlider.tsx
@@ -21,7 +21,7 @@ export default function HeroSlider({ locations, activeIndex, setActiveIndex }: H
     const interval = setInterval(() => {
       setActiveIndex((prev: number) => (prev + 1) % locations.length);
       animate(angle, angle.get() - 60, { duration: 1, ease: 'easeInOut' });
-    }, 3000);
+    }, 10000);
 
     return () => clearInterval(interval);
   }, [angle]);
@@ -81,13 +81,14 @@ export default function HeroSlider({ locations, activeIndex, setActiveIndex }: H
               <div className="relative flex items-center">
                 <motion.div
                   className={cn(
-                    "size-3 bg-white rounded-full relative",
-                    activeIndex === index && "after:content-[''] after:absolute after:left-1/2 after:top-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:size-8 after:border after:border-[#67B6B9] after:bg-primary/30 after:rounded-full after:z-[-1]"
+                    "size-3 bg-white rounded-full relative transition-all duration-300",
+                    "after:content-[''] after:opacity-0 after:absolute after:left-1/2 after:top-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:size-8 after:border after:border-[#67B6B9] after:bg-primary/30 after:rounded-full after:z-[-1] after:transition-all after:duration-300 after:delay-1000",
+                    activeIndex === index && "after:opacity-100"
                   )}
                 />
                 <div className={cn(
                   "ml-2 text-white text-xl font-semibold transition-all duration-300",
-                  activeIndex === index && "ml-6"
+                  activeIndex === index && "ml-6 delay-1000"
                 )}>
                   {location.name}
                 </div>
